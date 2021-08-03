41d7e5d40d73a6b1e1d422a087c1d223ff12a5f3
Ali Bulut <ali@alibulut.net>
2021-08-03 18:21:00 +0300

    creating WorkerService

---
+++ FileCreateWorkerService/Program.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FileCreateWorkerService.Services;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+
+namespace FileCreateWorkerService
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    IConfiguration Configuration = hostContext.Configuration;
+                    services.AddSingleton(sp => new ConnectionFactory() { Uri = new Uri(Configuration.GetConnectionString("RabbitMQ")), DispatchConsumersAsync = true });
+                    services.AddSingleton<RabbitMQClientService>();
+                    services.AddHostedService<Worker>();
+                });
+    }
+}
---
+++ FileCreateWorkerService/Services/RabbitMQClientService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+
+namespace FileCreateWorkerService.Services
+{
+    public class RabbitMQClientService
+    {
+        private readonly ConnectionFactory _connectionFactory;
+        private IConnection _connection;
+        private IModel _channel;
+        public static string QueueName = "queue-excel-file";
+        private readonly ILogger<RabbitMQClientService> _logger;
+
+        public RabbitMQClientService(ConnectionFactory connectionFactory, ILogger<RabbitMQClientService> logger)
+        {
+            _connectionFactory = connectionFactory;
+            _logger = logger;
+        }
+
+        public IModel Connect()
+        {
+            _connection = _connectionFactory.CreateConnection();
+            if (_channel?.IsOpen == true)
+            {
+                return _channel;
+            }
+
+            _channel = _connection.CreateModel();
+
+            _logger.LogInformation("Connection has been established with RabbitMQ.");
+
+            return _channel;
+        }
+
+        public void Dispose()
+        {
+            _channel?.Close();
+            _channel?.Dispose();
+            _channel = default;
+
+            _connection?.Close();
+            _connection?.Dispose();
+
+            _logger.LogInformation("Connection has been disposed with RabbitMQ.");
+        }
+    }
+}
